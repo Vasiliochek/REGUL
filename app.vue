9152d789020f6e17d6bf342c6b1f1a5a299f3de7
Vasily Makarov <makarov.vasia@gmail.com>
2024-11-12 13:31:24 +0400

    set up standart linting

--- app.vue
+++ app.vue
@@ -1,5 +1,11 @@
+<script setup lang="ts"></script>
+
 <template>
   <div>
+    <UButton
+      lable="BTN"
+      class="bg-black h-5 w-10"
+    />
     <NuxtRouteAnnouncer />
     <NuxtWelcome />
   </div>
