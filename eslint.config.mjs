1a8219dc4cd3ccdebf27b30804f3ad17a590ab5b
Vasily Makarov <makarov.vasia@gmail.com>
2024-11-15 12:33:01 +0400

    промежуточный

--- eslint.config.mjs
+++ eslint.config.mjs
@@ -1,5 +1,26 @@
 import withNuxt from './.nuxt/eslint.config.mjs'
 
 import eslintPluginPrettierRecommended from 'eslint-plugin-prettier/recommended'
+import unusedImports from 'eslint-plugin-unused-imports'
+import vueParser from 'vue-eslint-parser'
+import tsParser from '@typescript-eslint/parser'
 
-export default withNuxt(eslintPluginPrettierRecommended)
+export default withNuxt(eslintPluginPrettierRecommended, {
+  plugins: {
+    'unused-imports': unusedImports
+  },
+  languageOptions: {
+    parser: vueParser,
+    parserOptions: {
+      parser: tsParser
+    }
+  },
+  rules: {
+    'vue/multi-word-component-names': 0,
+    'vue/require-default-prop': 0,
+    'vue/no-v-html': 0,
+    'no-multi-spaces': 'error',
+    'no-console': 1,
+    'unused-imports/no-unused-imports-ts': 0
+  }
+})
--- nuxt.config.ts
+++ nuxt.config.ts
@@ -2,7 +2,7 @@
 export default defineNuxtConfig({
   compatibilityDate: '2024-04-03',
   devtools: { enabled: true },
-  modules: ['@nuxt/eslint'],
+  modules: ['@nuxt/eslint', '@nuxt/ui', '@vueuse/nuxt', '@pinia/nuxt'],
   eslint: {
     // Добавляем необходимые правила линта(мне честно дефолтных хватает)
   }
